7b7bbd5266a3c345dbcebb8ba865b77d0b551d64
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-07-22 20:38:00 +0430

    add serverInfo class based on excel headers and some extra detail for future

---
+++ src/src/Lib/ServerInfo.php
@@ -0,0 +1,47 @@
+<?php
+// src/Lib/ServerInfo.php
+declare(strict_types=1);
+
+namespace App\Lib;
+
+/**
+ *  Save each item of server detail
+ *
+ *  use php 8.1 Readonly Properties
+ *  use php 8.0 Constructor property promotion
+ */
+class ServerInfo
+{
+    // // get from model
+    // public readonly string $modelBrand;
+    // public readonly string $modelCpu;
+    // // get from ram
+    // public readonly int $ramCapacity;
+    // public readonly string $ramGen;
+    // // get from hdd
+    // public readonly int $hddCount;
+    // public readonly int $hddEachCapacity;
+    // public readonly int $hddTotalCapacity;
+    // public readonly string $hddType;
+    // // get from location
+    // public readonly string $locationCity;
+    // public readonly string $locationZone;
+    // public readonly string $locationCode;
+    // // get from price
+    // public readonly string $priceCurrency;
+    // public readonly int $priceAmount;
+
+    public function __construct(
+        public readonly int $index,
+        public readonly ?string $model,
+        public readonly ?string $ram,
+        public readonly ?string $hdd,
+        public readonly ?string $location,
+        public readonly ?string $price,
+      )
+      {
+        // calculate some other detail of each server and save them
+        // @todo calc
+
+      }
+}
