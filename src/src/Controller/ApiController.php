fe69bf27c7743aa859c95dbec634dfe6925ca72e
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-07-24 21:14:31 +0430

    fix sample links in api
    Signed-off-by: Javad Adib <Mr.Javad.Adib@gmail.com>

--- src/src/Controller/ApiController.php
+++ src/src/Controller/ApiController.php
@@ -26,7 +26,7 @@ class ApiController extends AbstractController
                     'pricing-hdd' => '/api/pricing?hdd=ssd',
                     'pricing-brand' => '/api/pricing?brand=HP',
                     'pricing-ram' => '/api/pricing?ram=8',
-                    'pricing-location' => '/api/pricing?location=AmsterdamAMS-01',
+                    'pricing-location' => '/api/pricing?location=AMS-01',
                     // one filter - multiple
                     'pricing-ram[multiple]' => '/api/pricing?ram=8|32|64',
                     'pricing-hdd[multiple]' => '/api/pricing?hdd=ssd|sas',
@@ -37,8 +37,8 @@ class ApiController extends AbstractController
                     'pricing-hdd-storage[range]' => '/api/pricing?hdd=ssd&storage=300-500',
                     'pricing-hdd[multiple]-storage[range]' => '/api/pricing?hdd=ssd|sas&storage=300-1000',
                     // all
-                    'pricing-hdd[multiple]-storage[range]-location-ram' => '/api/pricing?hdd=ssd|sas&storage=400-1000&AmsterdamAMS-01&ram=64',
-                    'pricing-hdd[multiple]-storage[range]-location-ram-brand[multiple]-price[range]' => '/api/pricing?hdd=ssd|sas&storage=400-1600&AmsterdamAMS-01&ram=96&brand=hp|dell&price=100-600',
+                    'pricing-hdd[multiple]-storage[range]-location-ram' => '/api/pricing?hdd=ssd|sas&storage=400-1000&AMS-01&ram=64',
+                    'pricing-hdd[multiple]-storage[range]-location-ram-brand[multiple]-price[range]' => '/api/pricing?hdd=ssd|sas&storage=400-1600&AMS-01&ram=96&brand=hp|dell&price=100-600',
 
                 ],
             ];
