45a55976a4fd76c75a4def369552ca2ef332866c
Javad Adib <Mr.Javad.Adib@gmail.com>
2022-07-24 14:52:11 +0430

    add some other unit test for server

--- src/tests/Lib/DataStructure/ServerTest.php
+++ src/tests/Lib/DataStructure/ServerTest.php
@@ -9,40 +9,43 @@ use PHPUnit\Framework\TestCase;
 
 final class ServerTest extends TestCase
 {
-    public function testServerConstructor(): void
+    public function testServerCheckAllAttributesExist(): void
     {
-        $this->assertClassHasAttribute('__construct', Server::class);
+        $this->assertClassHasAttribute('index', Server::class);
+        $this->assertClassHasAttribute('model', Server::class);
+        $this->assertClassHasAttribute('modelBrand', Server::class);
+        $this->assertClassHasAttribute('modelCpu', Server::class);
+        $this->assertClassHasAttribute('ram', Server::class);
+        $this->assertClassHasAttribute('ramCapacity', Server::class);
+        $this->assertClassHasAttribute('ramGen', Server::class);
+        $this->assertClassHasAttribute('hdd', Server::class);
+        $this->assertClassHasAttribute('hddCount', Server::class);
+        $this->assertClassHasAttribute('hddEachCapacity', Server::class);
+        $this->assertClassHasAttribute('hddTotalCapacity', Server::class);
+        $this->assertClassHasAttribute('hddType', Server::class);
+        $this->assertClassHasAttribute('location', Server::class);
+        $this->assertClassHasAttribute('locationCity', Server::class);
+        $this->assertClassHasAttribute('locationZone', Server::class);
+        $this->assertClassHasAttribute('locationCode', Server::class);
+        $this->assertClassHasAttribute('price', Server::class);
+        $this->assertClassHasAttribute('priceCurrency', Server::class);
+        $this->assertClassHasAttribute('priceAmount', Server::class);
     }
-
-    // public function testServerAttributeIndex(): void
-    // {
-    //     $this->assertClassHasAttribute('index', Server::class);
-    // }
-
-
-
-    // public function testCreateNewObjectFromServerByInvalidInputType(): void
-    // {
-    //     $args = $this->sampleObject();
-    //     $args['index'] = 'abc';
-    //     $this->assertIsObject(new Server(...$args));
-    // }
-
 
 
     /**
-     * @dataProvider sampleObject
+     * @dataProvider sampleDataProvider
      *
      * @return void
      */
-    public function testCreateNewObjectFromServerIsObject(array $args): void
+    public function testCreateNewObjectFromServerObjectWithDataProvider(array $args): void
     {
         $data = array_values($args);
         $this->assertIsObject(new Server(...$data));
     }
 
 
-    private function sampleObject(): array
+    public function sampleDataProvider(): array
     {
         return
             [
@@ -94,5 +97,63 @@ final class ServerTest extends TestCase
                     ]
                 ],
             ];
+    }
+
+
+
+    public function testCreateNewObjectFromServerObjectInvalidArray(): void
+    {
+        // expect type error because index is not int and string is passed
+        $this->expectError();
+
+        $data = array_values($this->sampleInvalidArray());
+        $this->assertIsObject(new Server(...$data));
+    }
+
+
+    public function sampleInvalidArray(): array
+    {
+        return
+            [
+                '1',
+                "Dell R210Intel Xeon X3440",
+                "Dell",
+                "X3440",
+                "16GBDDR3",
+                16,
+                "3",
+                "2x2TBSATA2",
+                2,
+                2000,
+                4000,
+                "SATA2",
+                "AmsterdamAMS-01",
+                "Amsterdam",
+                "AMS",
+                "01",
+                "€49.99",
+                "€",
+                49.99
+
+            ];
+    }
+
+
+    public function testCreateNewObjectFromServerWithOutData(): void
+    {
+        // expect type error because index is not int and string is passed
+        $this->expectError();
+
+        $this->assertIsObject(new Server());
+    }
+
+
+
+    public function testCreateNewObjectFromServerWithIncorrectData(): void
+    {
+        // expect type error because index is not int and string is passed
+        $this->expectError();
+
+        $this->assertIsObject(new Server(1, 2, 3));
     }
 }
